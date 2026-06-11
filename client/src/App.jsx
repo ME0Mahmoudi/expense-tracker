5cb5e9d7919e7a2e1598c79ee711d79120b841ba
ME0Mahmoudi <ME0Mahmoudi@gmail.com>
2026-06-11 15:21:48 +0330

    refactored: moved item-related states and actions(update and delete) to ExpenseItem.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,6 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const [expenses, setExpenses] = useState([]);
 
-  useEffect(() => {
-    loadExpenses();
-  }, []);
-
-  const [editingId, setEditingId] = useState(null)
-  const [editTitle, setEditTitle] = useState("")
-  const [editAmount, setEditAmount] = useState("")
-
   const loadExpenses = () => {
     fetch("http://localhost:3001/api/expenses")
       .then(res => res.json())
@@ -20,37 +12,9 @@ function App() {
       .catch(err => console.error(err));
   };
 
-  async function deleteExpense(id) {
-    await fetch(`http://localhost:3001/api/expenses/${id}`, {
-      method: "DELETE",
-    });
-
+  useEffect(() => {
     loadExpenses();
-  }
-
-  async function updateExpense(id) {
-    try {
-      await fetch(
-        `http://localhost:3001/api/expenses/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title: editTitle,
-            amount: Number(editAmount),
-          }),
-        }
-      )
-    } catch (err) {
-      console.error(err);
-
-    }
-
-    loadExpenses()
-    setEditingId(null)
-  }
+  }, []);
 
   return (
     <>
@@ -63,14 +27,7 @@ function App() {
           <ExpenseItem
             key={expense.id}
             expense={expense}
-            editingId={editingId}
-            editTitle={editTitle}
-            editAmount={editAmount}
-            setEditTitle={setEditTitle}
-            setEditAmount={setEditAmount}
-            setEditingId={setEditingId}
-            updateExpense={updateExpense}
-            deleteExpense={deleteExpense}
+            loadExpenses={loadExpenses}
           />
         ))}
       </ul>
--- client/src/components/ExpenseItem.jsx
+++ client/src/components/ExpenseItem.jsx
@@ -1,66 +1,97 @@
+import { useState } from "react";
+
 export default function ExpenseItem({
-    expense,
-    editingId,
-    editTitle,
-    editAmount,
-    setEditTitle,
-    setEditAmount,
-    setEditingId,
-    updateExpense,
-    deleteExpense,
-  }) {
-    if (editingId === expense.id) {
-      return (
-        <li>
-          <input
-            value={editTitle}
-            onChange={(e) => setEditTitle(e.target.value)}
-          />
-  
-          <input
-            type="number"
-            value={editAmount}
-            onChange={(e) => setEditAmount(e.target.value)}
-          />
-  
-          <button
-            onClick={() => updateExpense(expense.id)}
-          >
-            Save
-          </button>
-  
-          <button
-            onClick={() => {
-              setEditingId(null);
-              setEditTitle("");
-              setEditAmount("");
-            }}
-          >
-            Cancel
-          </button>
-        </li>
-      );
+  expense,
+  loadExpenses,
+}) {
+
+  const [isEditing, setIsEditing] = useState(false)
+  const [editTitle, setEditTitle] = useState(expense.title)
+  const [editAmount, setEditAmount] = useState(expense.amount)
+
+  async function updateExpense(id, editTitle, editAmount) {
+    try {
+      await fetch(
+        `http://localhost:3001/api/expenses/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: editTitle,
+            amount: Number(editAmount),
+          }),
+        }
+      )
+    } catch (err) {
+      console.error(err);
     }
-  
+
+    setIsEditing(false);
+    loadExpenses();
+  }
+
+  async function deleteExpense(id) {
+    await fetch(`http://localhost:3001/api/expenses/${id}`, {
+      method: "DELETE",
+    });
+
+    loadExpenses();
+  }
+
+  if (isEditing === true) {
     return (
       <li>
-        {expense.title} - ${expense.amount}
-  
+        <input
+          value={editTitle}
+          onChange={(e) => setEditTitle(e.target.value)}
+        />
+
+        <input
+          type="number"
+          value={editAmount}
+          onChange={(e) => setEditAmount(e.target.value)}
+        />
+
         <button
-          onClick={() => deleteExpense(expense.id)}
+          onClick={() => updateExpense(expense.id, editTitle, editAmount)}
         >
-          Delete
+          Save
         </button>
-  
+
         <button
           onClick={() => {
-            setEditingId(expense.id);
+            setIsEditing(false);
             setEditTitle(expense.title);
             setEditAmount(expense.amount);
           }}
         >
-          Edit
+          Cancel
         </button>
       </li>
     );
   }
+
+  return (
+    <li>
+      {expense.title} - ${expense.amount}
+
+      <button
+        onClick={() => deleteExpense(expense.id)}
+      >
+        Delete
+      </button>
+
+      <button
+        onClick={() => {
+          setIsEditing(true);
+          setEditTitle(expense.title);
+          setEditAmount(expense.amount);
+        }}
+      >
+        Edit
+      </button>
+    </li>
+  );
+}
